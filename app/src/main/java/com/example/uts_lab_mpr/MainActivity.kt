74553a3fa08bb28130ec904b7a81aae47a4e32cf
Puan Abidah Nitisara <puanabidah01@gmail.com>
2022-11-13 02:56:10 +0700

    membuat bottom navbar dan action bar

--- app/src/main/java/com/example/uts_lab_mpr/MainActivity.kt
+++ app/src/main/java/com/example/uts_lab_mpr/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.uts_lab_mpr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.uts_lab_mpr.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.mainContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        setupWithNavController(bottomNavigationView, navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu1 -> {
+                Toast.makeText(this, "Not Available yet", Toast.LENGTH_SHORT ).show()
+                return true
+            }
+            R.id.menu2 -> {
+                Toast.makeText(this, "Not Available yet", Toast.LENGTH_SHORT ).show()
+                return true
+            }
+            else -> return true
+        }
     }
 }
